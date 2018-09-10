954f3f4e09a788cd784fdd655676c6dc5142817b
byekei000 <byeeki000@warren.k12.in.us>
2018-09-09 22:12:43 -0400

    base game stuff

---
+++ src/tilegame/states/State.java
@@ -0,0 +1,17 @@
+package tilegame.states;
+
+import java.awt.*;
+
+public abstract class State {
+    //
+    private static State currentState = null;
+    public static void setState(State state){
+        currentState = state;
+    }
+    public static State getState(){
+        return currentState;
+    }
+    //
+    public abstract void tick();
+    public abstract void render(Graphics g);
+}
